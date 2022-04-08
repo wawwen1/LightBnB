3e9c04cfe8b0173885d57cd3becdd758615a1e01
warren roque <warrenroque17@gmail.com>
2022-04-08 00:16:45 +0000

    add sql query

--- lightbnb_webapp-master/server/database.js
+++ lightbnb_webapp-master/server/database.js
@@ -1,5 +1,13 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
 
 /// Users
 
@@ -67,12 +75,18 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
-}
+ return pool
+    .query(`
+    SELECT *
+    FROM properties
+    LIMIT $1`, [limit])
+    .then((res) => {
+      return res.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+  };    
 exports.getAllProperties = getAllProperties;
 
 
